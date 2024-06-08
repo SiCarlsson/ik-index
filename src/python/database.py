f255e20e8625762425d8135c4fb2cd1bb0db5718
Simon Carlsson <operator_ax.09@icloud.com>
2024-06-08 22:54:07 +0200

    file structure changes and scrapy added

--- src/python/database.py
+++ src/python/database.py
@@ -5,16 +5,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Database:
     def __init__(self):
         self.create_conn()
         self.create_tables()
 
-    ###
-    #   Creates a batabase if it not does not exists
-    ###
     def create_db(self):
+        """Creates a batabase if it not does not exists"""
         db_conn = mysql.connector.connect(
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
@@ -25,78 +22,67 @@ class Database:
             f"""CREATE DATABASE IF NOT EXISTS {os.getenv("DB_SCHEMA")}"""
         )
 
-    ###
-    #   Creates a connection and cursor with the database that is stored in the object
-    ###
     def create_conn(self):
-        # Makes sure that there is an existing database
+        """Creates a connection and cursor with the database that is stored in the object"""
         self.create_db()
+        self.establish_conn()
+        self.cursor = self.conn.cursor()
 
-        # Establishes wanted connections
+    def establish_conn(self):
+        """Establishes wanted connections"""
         self.conn = mysql.connector.connect(
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             host=os.getenv("DB_HOST"),
             database=os.getenv("DB_SCHEMA"),
         )
-        self.cursor = self.conn.cursor()
 
-    ###
-    #   Create tables in database if not yet exists
-    ###
     def create_tables(self):
-        # Issuers table
+        """Create tables in database if not yet exists"""
+
         self.cursor.execute(
             """CREATE TABLE IF NOT EXISTS Issuers (
                 id INT AUTO_INCREMENT PRIMARY KEY
             )"""
         )
 
-        # People table
         self.cursor.execute(
             """CREATE TABLE IF NOT EXISTS People (
                 id INT AUTO_INCREMENT PRIMARY KEY
             )"""
         )
 
-        # Roles Positions
         self.cursor.execute(
             """CREATE TABLE IF NOT EXISTS Roles (
                 id INT AUTO_INCREMENT PRIMARY KEY
             )"""
         )
 
-        # Transactions Positions
         self.cursor.execute(
             """CREATE TABLE IF NOT EXISTS Transactions (
                 id INT AUTO_INCREMENT PRIMARY KEY
             )"""
         )
 
-        # Dates Positions
         self.cursor.execute(
             """CREATE TABLE IF NOT EXISTS Dates (
                 id INT AUTO_INCREMENT PRIMARY KEY
             )"""
         )
 
-        # Instruments Positions
         self.cursor.execute(
             """CREATE TABLE IF NOT EXISTS Instruments (
                 id INT AUTO_INCREMENT PRIMARY KEY
             )"""
         )
 
-        # Currencies Positions
         self.cursor.execute(
             """CREATE TABLE IF NOT EXISTS Currencies (
                 id INT AUTO_INCREMENT PRIMARY KEY
             )"""
         )
 
-    ###
-    #   Close both the cursor and connection to the database
-    ###
     def close(self):
+        """Close both the cursor and connection to the database"""
         self.cursor.close()
         self.conn.close()
