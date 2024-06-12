60b524a447969ff44c89de3eb0c555a01c4bad20
Simon Carlsson <operator_ax.09@icloud.com>
2024-06-12 23:44:47 +0200

    MySqlPipeline creates database

--- src/python/webscraper/webscraper/pipelines.py
+++ src/python/webscraper/webscraper/pipelines.py
@@ -1,13 +1,44 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+import mysql.connector
+
+from mysql.connector import errorcode
+from scrapy.exceptions import DropItem
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+class MySqlPipeline:
 
+    def __init__(self):
+        self.host = os.getenv("DB_HOST")
+        self.user = os.getenv("DB_USER")
+        self.password = os.getenv("DB_PASSWORD")
+        self.database = os.getenv("DB_SCHEMA")
 
-class WebscraperPipeline:
+    def open_spider(self, spider):
+        self.check_db_exists()
+
+    def check_db_exists(self):
+        """Checks if the database exists and creates it if not"""
+        db_conn = mysql.connector.connect(
+            user=self.user,
+            password=self.password,
+            host=self.host,
+        )
+        db_conn.cursor().execute(
+            f"""CREATE DATABASE IF NOT EXISTS {os.getenv("DB_SCHEMA")}"""
+        )
+
+    def create_db_connection(self):
+        """Creates a connection to the database"""
+        self.conn = mysql.connector.connect(
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_SCHEMA"),
+        )
+        self.cursor = self.conn.cursor()
+
     def process_item(self, item, spider):
         return item
