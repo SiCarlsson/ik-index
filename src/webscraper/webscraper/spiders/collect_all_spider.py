5748eba14bf077036cdc2fdf1cdb8f588af08190
Simon Carlsson <operator_ax.09@icloud.com>
2024-06-30 01:18:25 +0200

    spider takes arguments

--- src/webscraper/webscraper/spiders/collect_all_spider.py
+++ src/webscraper/webscraper/spiders/collect_all_spider.py
@@ -1,8 +1,8 @@
 import scrapy
-import time
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from scrapy.http import Response
+from scrapy.exceptions import CloseSpider
 
 from ..items import WebscraperItem
 
@@ -14,44 +14,90 @@ class AllFinancialDataSpider(scrapy.Spider):
         "https://marknadssok.fi.se/publiceringsklient?page=1",
     ]
 
-    CURRENT_DATE = datetime.today().strftime("%Y-%m-%d")
-    MAXIMUM_PAGE_NUMBER = 0
-    CURRENT_PAGE_NUMBER = 1
+    def __init__(
+        self,
+        start_date: str = None,
+        end_date: str = None,
+        *args,
+        **kwargs,
+    ):
+        super(AllFinancialDataSpider, self).__init__(*args, **kwargs)
+        self.download_delay = 2
+
+        self.TODAY = datetime.today()
+
+        self.START_DATE = (
+            self._parse_date(start_date) if start_date else self._default_start_date()
+        )
+        self.END_DATE = (
+            self._parse_date(end_date) if end_date else self._default_end_date()
+        )
+
+        self.CURRENT_PAGE_NUMBER = 1
+
+    def _parse_date(self, date_str):
+        try:
+            year, month, day = map(int, date_str.split("-"))
+            return datetime(year, month, day).strftime("%Y-%m-%d")
+        except (ValueError, AttributeError):
+            raise ValueError(
+                f"Invalid date format: {date_str}. Expected format: 'YYYY-MM-DD'."
+            )
+
+    def _default_start_date(self):
+        return (self.TODAY - timedelta(days=1)).strftime("%Y-%m-%d")
+
+    def _default_end_date(self):
+        return datetime(2000, 1, 1).strftime("%Y-%m-%d")
 
     def parse(self, response: Response):
+        """Method is in charge of processing the response and returning scraped data and/or more URLs to follow.
+
+        Args:
+            response (Response): The response to parse
+
+        Raises:
+            CloseSpider: When the end-date is reached (or last page) the program is shut down
+
+        Yields:
+            scrapy.Item: Current scrapy item
+        """
         self.initialize_spider(response)
 
         for row in range(0, self.get_table_lenght(response)):
             item = self.extract_item(response, row)
 
-            if item["publication_date"] != self.CURRENT_DATE:
+            if item["publication_date"] == self.END_DATE:
+                raise CloseSpider("End date reached!")
+
+            if item["publication_date"] <= self.START_DATE:
                 yield item
 
-        if self.CURRENT_PAGE_NUMBER is not 15:
+        if self.CURRENT_PAGE_NUMBER < self.MAXIMUM_PAGE_NUMBER:
             next_page_url = f"https://marknadssok.fi.se/publiceringsklient?page={self.CURRENT_PAGE_NUMBER + 1}"
             if next_page_url is not None:
                 self.CURRENT_PAGE_NUMBER += 1
                 yield response.follow(next_page_url, callback=self.parse)
-
-        time.sleep(2)
+        else:
+            raise CloseSpider("Maximum page reached!")
 
     def initialize_spider(self, response: Response):
-        """Method holds logic that should only run when on the spiders first run
+        """Method holds logic that should only run once before the spider starts scraping
 
         Args:
             response (Response): The initial response of the spider
         """
         if self.CURRENT_PAGE_NUMBER == 1:
-            self.get_max_page_number(response)
+            self.MAXIMUM_PAGE_NUMBER = self.get_max_page_number(response)
 
     def get_max_page_number(self, response: Response):
-        """Method retrives the maximum amount of pages on the site
+        """Method gets and sets the maximum amount of pages on the site
 
         Args:
             response (Response): The response on the website
         """
 
-        self.MAXIMUM_PAGE_NUMBER = int(
+        return int(
             response.xpath(
                 '//*[@id="grid-list"]/div[2]/div/div/div[1]/ul/li[14]/a/text()'
             )
